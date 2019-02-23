cf3193dfcbfb976564991be291f827b92e956ca6
Hwld <0701hw@gmail.com>
2019-02-23 10:41:17 +0900

    ScrollViewを使用

---
+++ ScrollViewVer/ScrollViewVer/ScrollViewVer/Behaviors/DynamicStackLayoutBehavior.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScrollViewVer.Behaviors
+{
+    class DynamicStackLayoutBehavior : BehaviorBase<StackLayout>
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            "ItemsSource", typeof(ObservableCollection<string>), typeof(DynamicStackLayoutBehavior));
+
+        private bool IsInit = false;
+
+        public ObservableCollection<string> ItemsSource
+        {
+            get { return (ObservableCollection<string>)this.GetValue(ItemsSourceProperty); }
+            set { this.SetValue(ItemsSourceProperty, value); }
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            if (!IsInit)
+            {
+                ItemsSource.CollectionChanged += CollectionChange;
+                IsInit = true;
+            }
+        }
+
+        private void CollectionChange(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                AssociatedObject.Children.Add(new Label {
+                    Text = ItemsSource[e.NewStartingIndex],
+                    BackgroundColor = Color.Red,
+                    VerticalOptions = LayoutOptions.Center,
+                });
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                AssociatedObject.Children.RemoveAt(e.OldStartingIndex);
+            }
+        }
+    }
+}
---
+++ ScrollViewVer/ScrollViewVer/ScrollViewVer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ScrollViewVer.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> Items { get; } = new ObservableCollection<string>();
+        public ICommand AddItemCommand { get; }
+        public ICommand DeleteItemCommand { get; }
+
+        private Random RandomGenerator { get; set; } = new Random();
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            AddItemCommand = new Command(_ =>
+            {
+                Items.Add(Guid.NewGuid().ToString("N").Substring(0, RandomGenerator.Next(1, 30)));
+            });
+
+            DeleteItemCommand = new Command(_ =>
+            {
+                if (Items.Count > 0)
+                    Items.RemoveAt(Items.Count - 1);
+            });
+        }
+    }
+}
